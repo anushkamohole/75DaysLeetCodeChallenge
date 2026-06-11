38cf72a16f1cf517c76e98db178f667a24396bb6
anushkamohole <anushkamohole1@gmail.com>
2026-06-11 19:15:43 +0530

    Time: 0 ms (100%), Space: 8.2 MB (35.08%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        
+        string doubled = s + s;
+        
+        if (doubled.find(goal) != string::npos) {
+            return true;
+        }
+        
+        return false;
+    }
+};
