f52093e31019012b6c463a78083f0ee9fbff3a53
anushkamohole <anushkamohole1@gmail.com>
2026-06-11 15:48:58 +0530

    Time: 4 ms (34.31%), Space: 9.7 MB (49.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -14,3 +14,36 @@ public:
         return true;
     }
 };
+/* OPTIMIZED
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        // Create an inventory for the 26 lowercase English letters
+        int count[26] = {0}; 
+
+        // Make one single pass through both strings
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++; // 
+            count[t[i] - 'a']--; // 
+        }
+
+        // Check the inventory. If anything is NOT zero, it's not an anagram.
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};*/
+
