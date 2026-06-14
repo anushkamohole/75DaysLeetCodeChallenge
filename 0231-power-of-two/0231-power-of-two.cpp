c58a0fe8feb646474600f01a4da061ac6595bd04
anushkamohole <anushkamohole1@gmail.com>
2026-06-14 21:59:10 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.97%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,15 @@
-class Solution {
+/*class Solution {
 public:
     bool isPowerOfTwo(int n) {
         // __builtin_popcount counts the number of 1s in a 32-bit integer
         return n > 0 && __builtin_popcount(n) == 1;
+    }
+};
+*/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        // Must be greater than 0, AND the bitwise trick must equal 0
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
